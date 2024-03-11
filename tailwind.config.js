d0bd41b01c9efc87f02a8b8d0cf65676a7db76c4
tiberlium <lonew047@gmail.com>
2024-03-11 10:25:13 +0700

    perbaikan

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,46 +8,28 @@ module.exports = {
       lg: "976px",
       xl: "1440px",
     },
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      "blue-dark": "#0F172A",
-      "gray-light": "#d3dce6",
-      transparent: "transparent",
-      black: "#000",
-      white: "#fff",
-      gray: {
-        200: "#f7fafc",
-        800: "#d3dce6",
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
       },
-      fontFamily: {
-        sans: ["Graphik", "sans-serif"],
-        serif: ["Merriweather", "serif"],
-      },
-      extend: {
-        spacing: {
-          128: "32rem",
-          144: "36rem",
-        },
-        borderRadius: {
-          "4xl": "2rem",
-        },
-      },
-      container: {
-        padding: {
-          DEFAULT: "1rem",
-          sm: "2rem",
-          lg: "4rem",
-          xl: "5rem",
-          "2xl": "6rem",
-        },
+      borderRadius: {
+        "4xl": "2rem",
       },
     },
-    plugins: [require("preline/plugin")],
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
   },
+  plugins: [require("preline/plugin")],
 };
